9b2e839fac537dd78be81a81664bd57bb2629fab
Michelle Pan <mxpan@stanford.edu>
2014-02-05 00:55:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtualSpace::Application.config.secret_token = '62781111160c74e889f392d192bd0cc25e9c69cc22b21e836648b8305736d22d56023707ca3f420dddc7c5ff6d8df1a0cf8e69963ae5dd9fae1d2cb4d2219d69'
