7bfc9eec23a6a10ae31d855dbd2f35633cbac3dc
Matthew Pick <mpick@stanford.edu>
2014-02-05 19:29:15 -0800

    moved room page from improper location in inspire

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def inspire
-  	@username = "Jon";
-  	@itemsArr = [{:name => "apple", :description => "this is an apple", :imageURL => "http://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Culinary_fruits_front_view.jpg/1280px-Culinary_fruits_front_view.jpg"}]
-  	@itemsArr2 = [{:name => "apple", :description => "this is an apple", :imageURL => "http://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Culinary_fruits_front_view.jpg/1280px-Culinary_fruits_front_view.jpg"}, {:name => "banana", :description => "this is an banana", :imageURL => "http://thepaleodiet.com/wp-content/uploads/2013/05/fruit.jpg"}]
+ 
   end
 end
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,7 @@
 class SpacesController < ApplicationController
+	def index
+		@username = "Jon";
+  		@itemsArr = [{:name => "apple", :description => "this is an apple", :imageURL => "http://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Culinary_fruits_front_view.jpg/1280px-Culinary_fruits_front_view.jpg"}]
+  		@itemsArr2 = [{:name => "apple", :description => "this is an apple", :imageURL => "http://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Culinary_fruits_front_view.jpg/1280px-Culinary_fruits_front_view.jpg"}, {:name => "banana", :description => "this is an banana", :imageURL => "http://thepaleodiet.com/wp-content/uploads/2013/05/fruit.jpg"}]
+	end
 end
