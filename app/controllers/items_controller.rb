083a12650d0152ac6922c2892e84fba4cb07242d
Michelle Pan <mxpan@stanford.edu>
2014-02-12 23:19:45 -0800

    add ability to add tags when creating rooms/items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,56 @@
 class ItemsController < ApplicationController
 	def index
 		@item = Item.find_by_id(params[:id])
-		
-		#@items = [{:name => "French Table", :id => "item01", :description => "what a wonderfully UGLY table", :imageURL => "/images/item01.jpg"}, {:name => "Bed", :id => "item02", :description => "nothin' like a classic", :imageURL => "/images/item02.jpg"}, {:name => "New Crib", :id => "item03", :description => "new crib for my new crib", :imageURL => "/images/item03.jpg"}]
 	end
 
 	def create
 		@item = Item.new
-		if !params[:item][:imageURL]
+		if !params[:imageURL]
 			flash[:item_error] = "No file chosen"
 			redirect_to :controller => "spaces", :action => "add"
-		elsif params[:item][:name] == ""
+		elsif params[:name] == ""
 			flash[:item_error] = "You must enter a name"
 			redirect_to :controller => "spaces", :action => "add"
 		else
-			uploaded_io = params[:item][:imageURL]
+			uploaded_io = params[:imageURL]
 			File.open(Rails.root.join('public', 'images', uploaded_io.original_filename), 'wb') do |file|
 	    			file.write(uploaded_io.read)
 	  		end
 
-	  		@item.name = params[:item][:name]
-	  		@item.description = params[:item][:description]
+	  		if params[:tag1] != ""
+	  			tag1 = Tag.find_by_name(params[:tag1])
+	  			if tag1.nil?
+	  				tag1 = Tag.new
+	  				tag1.name = params[:tag1]
+	  				tag1.save
+	  			end
+	  			@item.tags << tag1
+	  		end
+
+	  		if params[:tag2] != ""
+	  			tag2 = Tag.find_by_name(params[:tag2])
+	  			if tag2.nil?
+	  				tag2 = Tag.new
+	  				tag2.name = params[:tag2]
+	  				tag2.save
+	  			end
+	  			@item.tags << tag2
+	  		end
+
+	  		if params[:tag3] != ""
+	  			tag3 = Tag.find_by_name(params[:tag3])
+	  			if tag3.nil?
+	  				tag3 = Tag.new
+	  				tag3.name = params[:tag3]
+	  				tag3.save
+	  			end
+	  			@item.tags << tag3
+	  		end
+
+	  		user = User.find_by_id(session[:user_id])
+	  		@item.user = user
+	  		@item.name = params[:name]
+	  		@item.description = params[:description]
 	  		@item.imageURL = uploaded_io.original_filename
 	  		@item.borrowedTimes = 0
 	  		@item.save
