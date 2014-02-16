dbb7135d34a18ab615278f31e564f99316485908
Matthew Pick <mpick@stanford.edu>
2014-02-16 13:58:11 -0800

    added searching by name and changed iteration to just sql queries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,23 +15,25 @@ class SearchController < ApplicationController
 	  	@foundItems = []
 
 		if searchString != ""
-			#find rooms with tags containing substring
-			tags = Tag.find(:all)
-			tags.each do |tag|
-				if tag.name.downcase.include?(searchString)
-					rooms = tag.rooms
-					rooms.each do |room|
-						if (!@foundRooms.include?(room))
-							@foundRooms << room
-						end
-					end
+			# find rooms with tags containing substring
 
-					items = tag.items
-					items.each do |item|
-						if (!@foundItems.include?(item))
-							@foundItems << item
-						end
-					end
+			rooms = Room.find_by_sql("SELECT * from Rooms where name like '%"+searchString+"%'")
+			@foundRooms = @foundRooms + rooms
+
+			items = Item.find_by_sql("SELECT * from Items where name like '%"+searchString+"%'")
+			@foundItems = @foundItems + items
+
+			roomTags = Room.find_by_sql("SELECT * from rooms r where r.id in (SELECT r.id from rooms r, rooms_tags, tags t where r.id = rooms_tags.room_id and t.id = rooms_tags.tag_id and t.name like '%"+searchString+"%')")
+			roomTags.each do |rt|
+				if !@foundRooms.include?(rt)
+					@foundRooms << rt
+				end
+			end
+
+			itemTags = Item.find_by_sql("SELECT * from items i where i.id in (SELECT i.id from items i, items_tags, tags t where i.id = items_tags.item_id and t.id = items_tags.tag_id and t.name like '%"+searchString+"%')")
+			itemTags.each do |it|
+				if !@foundItems.include?(it)
+					@foundItems << it
 				end
 			end
 		end
