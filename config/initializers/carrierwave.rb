9383320973d178a39e996f249be74f6eadee8081
Michelle Pan <mxpan@stanford.edu>
2014-03-06 15:44:46 -0800

    change permissions on aws iam

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     :provider               => 'AWS',
-#     :aws_access_key_id      => 'AKIAJHJQBIIIPBEGS2SQ', 
-#     :aws_secret_access_key  => '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',
-#     :region                 => 'us-west-1', # optional, defaults to 'us-east-1'
-#   }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAJKKFGUUSH4B2ZL5A', 
+    :aws_secret_access_key  => '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',
+    :region                 => 'us-west-1', # optional, defaults to 'us-east-1'
+  }
 
-#   config.fog_directory  = 'vsapp' # required
-# end
+  #Required for Heroku
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_directory  = 'vsapp' # required
+end
