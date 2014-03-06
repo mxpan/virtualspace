b0228a9d6cc0d3b1d195f2cc198d0582fa40803c
Michelle Pan <mxpan@stanford.edu>
2014-03-06 14:40:33 -0800

    remove fog host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAJHJQBIIIPBEGS2SQ',       # required
     :aws_secret_access_key  => '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', # required
     :region                 => 'us-west-1', # optional, defaults to 'us-east-1'
-    :host                   => 's3-us-west-1.amazonaws.com', # optional, defaults to nil
+    :host                   => '',             # optional, defaults to nil
     :endpoint               => '' # optional, defaults to nil
   }
 
-  config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
-  config.fog_directory = 'vsapp'
+  config.fog_directory  = 'vsapp'                             # required
 end
