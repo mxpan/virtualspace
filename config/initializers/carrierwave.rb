6b0f2d9150ca0a38af8fe2da1998fa74eb20bd6f
Michelle Pan <mxpan@stanford.edu>
2014-03-06 14:11:45 -0800

    get rid of s3 access policy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp') # adding these...
   config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
-  config.s3_access_policy = :public_read
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => 'AKIAJHJQBIIIPBEGS2SQ',       # required
     :aws_secret_access_key  => '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', # required
-    :region                 => '',                  # optional, defaults to 'us-east-1'
-    :host                   => '',             # optional, defaults to nil
+    :region                 => 'us-west-1', # optional, defaults to 'us-east-1'
+    :host                   => '', # optional, defaults to nil
     :endpoint               => '' # optional, defaults to nil
   }
   config.fog_directory  = 'vsapp'
+  config.fog_host = "https://s3-us-west-1.amazonaws.com/vsapp"
 end
