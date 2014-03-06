7f2edacb021f40e41c3749aa12eefba7d5216f3d
Michelle Pan <mxpan@stanford.edu>
2014-03-06 14:31:26 -0800

    remove fog hsot

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,10 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => 'AKIAJHJQBIIIPBEGS2SQ',       # required
     :aws_secret_access_key  => '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', # required
     :region                 => 'us-west-1', # optional, defaults to 'us-east-1'
-    :host                   => '', # optional, defaults to nil
+    :host                   => 's3-us-west-1.amazonaws.com', # optional, defaults to nil
     :endpoint               => '' # optional, defaults to nil
   }
 
   config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
   config.fog_directory = 'vsapp'
-  config.fog_host = 'https://s3-us-west-1.amazonaws.com/vsapp'
 end
