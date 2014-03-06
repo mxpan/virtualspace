4778c9ae2464bba30f3cf5d09ba25dbc74719e0a
Michelle Pan <mxpan@stanford.edu>
2014-03-05 23:38:34 -0800

    move images to subdir

--- app/uploaders/image_upload_uploader.rb
+++ app/uploaders/image_upload_uploader.rb
@@ -13,8 +13,8 @@ class ImageUploadUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    #"images"
+    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "images"
   end
 
   process :resize_to_fill => [300, 300]
