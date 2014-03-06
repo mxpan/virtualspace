c5496e5c04192e0e52858bc569e01908cb4a53a9
Michelle Pan <mxpan@stanford.edu>
2014-03-06 14:45:10 -0800

    different carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,11 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
-
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJHJQBIIIPBEGS2SQ',       # required
-    :aws_secret_access_key  => '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', # required
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAJHJQBIIIPBEGS2SQ', 
+    :aws_secret_access_key  => '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',
     :region                 => 'us-west-1', # optional, defaults to 'us-east-1'
-    :host                   => '',             # optional, defaults to nil
-    :endpoint               => '' # optional, defaults to nil
   }
-
-  config.fog_directory  = 'vsapp'                             # required
+ 
+  config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
+  config.fog_directory    = 'vsapp'
 end
