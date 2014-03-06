595cdb8988c6ccd8e93c1a1755d70873288eb719
Michelle Pan <mxpan@stanford.edu>
2014-03-05 22:28:07 -0800

    change files to poopnuggets.jpg to test

--- app/uploaders/image_upload_uploader.rb
+++ app/uploaders/image_upload_uploader.rb
@@ -55,7 +55,8 @@ class ImageUploadUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-     "#{secure_token}.#{file.extension}" if original_filename.present?
+    "poopnuggets.jpg" if original_filename.present?
+    #"#{secure_token}.#{file.extension}" if original_filename.present?
   end
 
   protected
