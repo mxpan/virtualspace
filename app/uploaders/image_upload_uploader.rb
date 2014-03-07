d02dc608712940c971b496c086a84bec8db6d472
Michelle Pan <mxpan@stanford.edu>
2014-03-06 16:05:35 -0800

    holy shit it finally works. my eyes hurt.

--- app/uploaders/image_upload_uploader.rb
+++ app/uploaders/image_upload_uploader.rb
@@ -7,7 +7,6 @@ class ImageUploadUploader < CarrierWave::Uploader::Base
   permissions 0777
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
